33a93c607aaa93b7622ced470104360bd56a7175
Xrohitahir <xrohitahir@gmail.com>
2024-12-16 13:35:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
+API_ID = int(getenv("API_ID", "24497147"))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = int(getenv("OWNER_ID", ""))
