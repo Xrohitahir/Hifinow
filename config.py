61188f3468aa62a7ca192ac663a8c349ec22b543
Xrohitahir <xrohitahir@gmail.com>
2024-12-16 13:10:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
+API_ID = int(getenv("API_ID", "24497147"))
+API_HASH = getenv("API_HASH", "350b3b383bd453a2060d9dcccb9e31cb")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8176451634").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://irohitahir:1CrmW6nYeQ8nCP2G@cluster0.c2in5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002347610797")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002182888803"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
