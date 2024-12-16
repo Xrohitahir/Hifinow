968ff6bca27220e89636eb05f9f8cb7ea8dea859
Xrohitahir <xrohitahir@gmail.com>
2024-12-16 17:43:55 +0530

    Update script.py

--- devgagan/core/script.py
+++ devgagan/core/script.py
@@ -3,7 +3,7 @@
 # ------------------------------------------------------------ #
 
 START_TXT = """
-Hi, welcome to Advance Content Saver Bot, designed to save restricted messages from public/private channels and private groups. First login in bot by /login then send post link.
+Hi, welcome to Save Content Advance Bot, designed to save restricted messages from public/private channels and private groups. First login in bot by /login then send post link.
 """
 
 FORCE_MSG = """
