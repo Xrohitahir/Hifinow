b66c5ce53d1abe3ad3fce82b39d15e176b9b9532
Xrohitahir <xrohitahir@gmail.com>
2025-01-18 01:00:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,4 +11,4 @@ MONGO_DB = getenv("MONGO_DB", "mongodb+srv://irohitahir:1CrmW6nYeQ8nCP2G@cluster
 LOG_GROUP = getenv("LOG_GROUP", "-1002347610797")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002182888803"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000"))
