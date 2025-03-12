3ffc483a15be957f6f8eddb10cad170f3612bb8a
Xrohitahir <xrohitahir@gmail.com>
2025-03-12 20:31:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "244147"))
-API_HASH = getenv("API_HASH", "350b3b383bd453a2060d9dcccb9ecb")
+API_ID = int(getenv("API_ID", "27149841"))
+API_HASH = getenv("API_HASH", "109c63a1971283c71bc228bca5a63c6d")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "8176451634").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://rohityadavbots:EhXvJKdpq6FdzHO2@cluster0.x4ika.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002347610797")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002182888803"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000"))
+PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "1000"))
